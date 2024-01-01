1bbb47a8a6d5df6083fc21e0cb413708a3b5fba4
Chase Wu <minowu@foxmail.com>
2024-01-01 09:52:02 +0800

    Initial commit

---
+++ apps/web/src/app/app.tsx
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+import NxWelcome from './nx-welcome';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return (
+    <StyledApp>
+      <NxWelcome title="web" />
+    </StyledApp>
+  );
+}
+
+export default App;
