65f306c98a4eb232844f09d9a85f16463e424991
Chase Wu <minowu@foxmail.com>
2024-01-30 15:52:54 +0800

    chore: move files

--- apps/core/prisma-client-basic/jest.config.ts
+++ apps/core/prisma-client-basic/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
   displayName: 'prisma-client-basic',
-  preset: '../../jest.preset.js',
+  preset: '../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/prisma-client-basic',
+  coverageDirectory: '../../../coverage/apps/core/prisma-client-basic',
 };
