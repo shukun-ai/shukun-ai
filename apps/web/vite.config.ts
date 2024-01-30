b3af191e2644ffebddcad584fd6f88f18d9a835e
Chase Wu <minowu@foxmail.com>
2024-01-30 14:49:15 +0800

    chore: add checker plugin for vite

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import checker from 'vite-plugin-checker';
 
 export default defineConfig({
   root: __dirname,
@@ -17,7 +18,16 @@ export default defineConfig({
     host: 'localhost',
   },
 
-  plugins: [react(), nxViteTsPaths()],
+  plugins: [
+    react(),
+    nxViteTsPaths(),
+    checker({
+      typescript: {
+        root: `${process.cwd()}/apps/web`,
+        tsconfigPath: 'tsconfig.app.json',
+      },
+    }),
+  ],
 
   // Uncomment this if you are using workers.
   // worker: {
