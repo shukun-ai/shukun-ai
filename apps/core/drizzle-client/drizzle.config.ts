9307b0912eae6ced3a11854e0a712ecea70ac31c
Chase Wu <minowu@foxmail.com>
2024-02-18 20:24:43 +0800

    chore: update db:generate

--- apps/core/drizzle-client/drizzle.config.ts
+++ apps/core/drizzle-client/drizzle.config.ts
@@ -1,12 +1,10 @@
 import type { Config } from 'drizzle-kit';
 import { resolve } from 'node:path';
-const connectionString = '';
 
 export default {
   driver: 'pg',
   out: './apps/core/drizzle-client/src/migration',
-  schema: [resolve(__dirname, './apps/core/drizzle-client/src/schema.ts')],
-  dbCredentials: { connectionString },
+  schema: [resolve(__dirname, './src/schema.ts')],
   verbose: true,
   strict: true,
 } satisfies Config;
