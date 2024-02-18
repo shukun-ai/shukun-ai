e26a7e151b9af3745a085a655d6432da9f072925
Chase Wu <minowu@foxmail.com>
2024-02-18 20:59:43 +0800

    chore: update dockerfile and add docker-compose

--- docker/build-amd64.sh
+++ docker/build-amd64.sh
@@ -1,3 +1,3 @@
 cd ..
-DOCKER_BUILDKIT=1 docker build -t shukun-ai/app:latest --platform=linux/amd64 --no-cache -f Dockerfile --target app .
-DOCKER_BUILDKIT=1 docker build -t shukun-ai/web:latest --platform=linux/amd64 --no-cache -f Dockerfile --target web .
+DOCKER_BUILDKIT=1 docker build -t shukunai/app:latest --platform=linux/amd64 --no-cache -f Dockerfile --target app .
+DOCKER_BUILDKIT=1 docker build -t shukunai/web:latest --platform=linux/amd64 --no-cache -f Dockerfile --target web .
